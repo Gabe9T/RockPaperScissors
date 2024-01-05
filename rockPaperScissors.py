7862df035f901ae7e41ca1d5711808e9f8574afc
gabe <gabrieltucker260@gmail.com>
2024-01-04 19:10:22 -0800

    made interface

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,1 +1,17 @@
-print("inital commit")
+import random 
+
+user_wins = 0
+computer_wins = 0
+
+playable_choices = ["rock", "paper", "scissors", "r", "p", "s"]
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in playable_choices:
+        continue
+
+    random_number = random.randint(0, 5)
+
+    
